d3345054a46831e88e1f86a982982aa0a4b00d55
mre55 <misganawmihreteab>
2022-03-02 23:23:43 +0300

    Created the has-many/belongs-to r/ship b/n Classroom and Student

--- school/student.rb
+++ school/student.rb
@@ -1,3 +1,5 @@
+require_relative 'classroom'
+
 class Student < Person
   def initialize(age, _name, _parent_permission, classroom: nil)
     super(age, name: 'Unknow', parent_permission: true)
@@ -6,5 +8,10 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
